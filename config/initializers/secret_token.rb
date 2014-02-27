9d495a3fd191a1aa9bd173ee065e659ec0fc008c
Antonin Amand <antonin.amand@gmail.com>
2014-02-27 12:45:42 +0100

    init engine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngineLba::Application.config.secret_token = 'd87b42fa7a8bed05f7e1b3cc6d97f4be69a342b4682309f63664be0ef85d0cea7ead86534abcc63785ae80c3ddce90e751045d55d9c45ad38390f67367d9721e'
